4134696787f8af37923421c6ab56f85bc2a91828
Thiran Sasanka <thiransasanka1@gmail.com>
2024-08-15 02:22:54 +0530

    marketplace

--- lib/screen/marketplace.dart
+++ lib/screen/marketplace.dart
@@ -1,7 +1,10 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
+import '../models/marketplace_model.dart';
 import '../theme/app_theme.dart';
+import 'marketplace_item_details.dart';
 
 class Marketplace extends StatefulWidget {
   const Marketplace({super.key});
@@ -13,9 +16,111 @@ class Marketplace extends StatefulWidget {
 class _HomeScreenState extends State<Marketplace> {
   String? _selectedItem;
   final List<String> _items = ['Equipments', 'Accommodation', 'Food & Restaurants', 'Vehicle'];
+  final List<MarketplaceItem> items = [
+    MarketplaceItem(
+      title: "Item 1",
+      imageUrls: [
+        "https://via.placeholder.com/150",
+        "https://via.placeholder.com/200",
+        "https://via.placeholder.com/250",
+        "https://via.placeholder.com/300",
+      ],
+      price: "\$100",
+      description: 'This is the item description',
+      location: 'Colombo',
+    ),
+
+    MarketplaceItem(
+      title: "Item 1",
+      imageUrls: [
+        "https://via.placeholder.com/150",
+        "https://via.placeholder.com/200",
+        "https://via.placeholder.com/250",
+        "https://via.placeholder.com/300",
+      ],
+      price: "\$100",
+      description: 'This is the item description',
+      location: 'Colombo',
+    ), MarketplaceItem(
+      title: "Item 1",
+      imageUrls: [
+        "https://via.placeholder.com/150",
+        "https://via.placeholder.com/200",
+        "https://via.placeholder.com/250",
+        "https://via.placeholder.com/300",
+      ],
+      price: "\$100",
+      description: 'This is the item description',
+      location: 'Colombo',
+    ),
+    MarketplaceItem(
+      title: "Item 1",
+      imageUrls: [
+        "https://via.placeholder.com/150",
+        "https://via.placeholder.com/200",
+        "https://via.placeholder.com/250",
+        "https://via.placeholder.com/300",
+      ],
+      price: "\$100",
+      description: 'This is the item description',
+      location: 'Colombo',
+    ),
+    MarketplaceItem(
+      title: "Item 1",
+      imageUrls: [
+        "https://via.placeholder.com/150",
+        "https://via.placeholder.com/200",
+        "https://via.placeholder.com/250",
+        "https://via.placeholder.com/300",
+      ],
+      price: "\$100",
+      description: 'This is the item description',
+      location: 'Colombo',
+    ),
+    MarketplaceItem(
+      title: "Item 1",
+      imageUrls: [
+        "https://via.placeholder.com/150",
+        "https://via.placeholder.com/200",
+        "https://via.placeholder.com/250",
+        "https://via.placeholder.com/300",
+      ],
+      price: "\$100",
+      description: 'This is the item description',
+      location: 'Colombo',
+    ),
+    MarketplaceItem(
+      title: "Item 1",
+      imageUrls: [
+        "https://via.placeholder.com/150",
+        "https://via.placeholder.com/200",
+        "https://via.placeholder.com/250",
+        "https://via.placeholder.com/300",
+      ],
+      price: "\$100",
+      description: 'This is the item description',
+      location: 'Colombo',
+    ),
+    MarketplaceItem(
+      title: "Item 1",
+      imageUrls: [
+        "https://via.placeholder.com/150",
+        "https://via.placeholder.com/200",
+        "https://via.placeholder.com/250",
+        "https://via.placeholder.com/300",
+      ],
+      price: "\$100",
+      description: 'This is the item description',
+      location: 'Colombo',
+    ),
+
+
+    // Add more items here...
+  ];
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       body: Container(
         width: double.infinity,
@@ -24,13 +129,6 @@ class _HomeScreenState extends State<Marketplace> {
           scrollDirection: Axis.vertical,
           child: Column(
             children: [
-              Text(
-                'Marketplace',
-                style: TextStyle(
-                    fontSize: 18,
-                    color: AppTheme.colors.primary_dark_3,
-                    fontWeight: FontWeight.bold),
-              ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 // crossAxisAlignment: CrossAxisAlignment.center,
@@ -112,7 +210,78 @@ class _HomeScreenState extends State<Marketplace> {
                   ),
                 ],
               ),
-              SizedBox(height: 100,)
+              //marketplace grid
+              Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      alignment: Alignment.topCenter,
+                      height: size.height * 0.68,
+                      child: GridView.builder(
+                        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2, // Number of columns
+                          childAspectRatio: 0.75, // Aspect ratio for the items
+                          crossAxisSpacing: 10,
+                          mainAxisSpacing: 8.0,
+                        ),
+                        itemCount: items.length,
+                        itemBuilder: (context, index) {
+                          final item = items[index];
+                          return GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => MarketplaceItemDetailPage(item: item),
+                                ),
+                              );
+                            },
+                            child: Card(
+                              color: AppTheme.colors.secondary_light_1,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(16.0),
+                              ),
+                              clipBehavior: Clip.antiAlias,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Expanded(
+                                    child: Hero(
+                                      tag: item.imageUrls.first, // Use the first image URL as the hero tag
+                                      child: CachedNetworkImage(
+                                        imageUrl: item.imageUrls.first, // Show only the first image
+                                        placeholder: (context, url) => const Center(child: CircularProgressIndicator()),
+                                        errorWidget: (context, url, error) => Icon(Icons.error),
+                                        fit: BoxFit.cover,
+                                        width: double.infinity,
+                                      ),
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Text(
+                                      item.title,
+                                      style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(left: 8.0, bottom: 8),
+                                    child: Text(
+                                      item.price,
+                                      style: TextStyle(fontSize: 14.0, color: Colors.green),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
---
+++ lib/screen/marketplace_item_details.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import '../models/marketplace_model.dart';
+
+class MarketplaceItemDetailPage extends StatefulWidget {
+  final MarketplaceItem item;
+
+  const MarketplaceItemDetailPage({super.key, required this.item});
+
+  @override
+  MarketplaceItemDetailPageState createState() =>
+      MarketplaceItemDetailPageState();
+}
+
+class MarketplaceItemDetailPageState extends State<MarketplaceItemDetailPage> {
+  late PageController _pageController;
+  int _currentPage = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(initialPage: _currentPage);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.white, // Set the AppBar background to white
+        elevation: 2, // Adds a subtle shadow to the AppBar
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back), // Customize the back button icon
+          color: Colors.black, // Set the back button color to black
+          onPressed: () =>
+              Navigator.of(context).pop(), // Go back to the previous screen
+        ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            child: Stack(
+              alignment: Alignment.bottomCenter,
+              children: [
+                PageView.builder(
+                  controller: _pageController,
+                  itemCount: widget.item.imageUrls.length,
+                  onPageChanged: (index) {
+                    setState(() {
+                      _currentPage = index;
+                    });
+                  },
+                  itemBuilder: (context, index) {
+                    return Hero(
+                      tag: widget.item.imageUrls[index],
+                      child: CachedNetworkImage(
+                        imageUrl: widget.item.imageUrls[index],
+                        placeholder: (context, url) =>
+                            Center(child: CircularProgressIndicator()),
+                        errorWidget: (context, url, error) => Icon(Icons.error),
+                        fit: BoxFit.cover,
+                        width: double.infinity,
+                        height: double.infinity,
+                      ),
+                    );
+                  },
+                ),
+                Positioned(
+                  bottom: 16.0,
+                  child: Container(
+                    alignment: Alignment.center,
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                    decoration: BoxDecoration(
+                      color: Colors.black54,
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                    child: Text(
+                      "${_currentPage + 1}/${widget.item.imageUrls.length}",
+                      style: TextStyle(color: Colors.white, fontSize: 16.0),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(16.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  widget.item.title,
+                  style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 8.0),
+                Text(
+                  widget.item.price,
+                  style: TextStyle(fontSize: 20.0, color: Colors.green),
+                ),
+                SizedBox(height: 8.0),
+                Text(
+                  "Location: ${widget.item.location ?? 'Unknown Location'}",
+                  style: TextStyle(fontSize: 16.0, color: Colors.grey[700]),
+                ),
+                SizedBox(height: 16.0),
+                Text(
+                  "Description",
+                  style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 8.0),
+                Text(
+                  widget.item.description ?? 'No description available.',
+                  style: TextStyle(fontSize: 16.0),
+                ),
+                SizedBox(height: 24.0),
+                Center(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Implement navigation to chat or messaging screen here
+                    },
+                    style: ElevatedButton.styleFrom(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: 24.0, vertical: 12.0),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                    ),
+                    child: Text(
+                      "Message Seller",
+                      style: TextStyle(fontSize: 16.0),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
